219f2398449ac991c5a12ec9d88e0471ef9dc6e0
Ray Olivier <rayolivier@outlook.com>
2023-12-28 19:46:53 -0600

    authorization flow

--- index.js
+++ index.js
@@ -1,16 +1,129 @@
+require('dotenv').config();
 const express = require('express');
+const querystring = require('querystring');
 
 const app = express();
+const axios = require('axios');
 const port = 8080;
 
-app.get('/', (req, res) => {
-    const data = {
-        text: 'Hello World!'
-    }
-    res.json(data);
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
-})
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+const generateRandomString = length => {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+const stateKey = 'spotify_auth_state';
+
+app.get('/', (req, res) => {
+  const data = {
+    text: 'Hello World!'
+  };
+  res.json(data);
+});
+
+app.get('/login', (req, res) => {
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+  const scope = 'user-read-private user-read-email';
+
+  const queryParams = querystring.stringify({
+    client_id: CLIENT_ID,
+    response_type: 'code',
+    redirect_uri: REDIRECT_URI,
+    state: state,
+    scope: scope
+  });
+
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+});
+
+app.get('/callback', (req, res) => {
+  // pull authorization code from Spotify's redirect
+  const code = req.query.code || null;
+
+  // POST to exchange authorization code for access token
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: querystring.stringify({ grant_type: 'authorization_code', code: code, redirect_uri: REDIRECT_URI }),
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`
+    }
+  })
+    .then(response => {
+      if (response.status === 200) {
+        const { access_token, token_type } = response.data;
+
+        // // testing by grabbing profile info
+        // axios
+        //   .get('https://api.spotify.com/v1/me', {
+        //     headers: {
+        //       Authorization: `${token_type} ${access_token}`
+        //     }
+        //   })
+        //   .then(response => {
+        //     res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+        //   })
+        //   .catch(err => res.send(err));
+
+        // testing refresh token route
+        const { refresh_token } = response.data;
+        axios
+          .get(`http://localhost:8080/refresh_token?refresh_token=${refresh_token}`)
+          .then(response => {
+            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+          })
+          .catch(error => {
+            res.send(error);
+          });
+      } else {
+        res.send(response);
+      }
+    })
+    .catch(error => {
+      res.send(error);
+    });
+});
+
+/**
+ * Refresh access token if expired
+ */
+app.get('/refresh_token', (req, res) => {
+  const { refresh_token } = req.query;
+
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: querystring.stringify({
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    }),
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`
+    }
+  })
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(error => {
+      res.send(error);
+    });
+});
 
 app.listen(port, () => {
-    console.log(`Express app listening at  http://localhost:${port}`)
-})
+  console.log(`Express app listening at  http://localhost:${port}`);
+});
