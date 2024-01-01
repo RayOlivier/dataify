422e1d6a3b422c6983e47df9261bd919ca3954a6
Ray Olivier <rayolivier@outlook.com>
2024-01-01 14:50:37 -0600

    set up proxy for local server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.scss'
+import { useEffect, useState } from 'react';
+import './App.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
+  console.log('app');
+
+  // TEST proxy
+  // const fetchData = async () => {
+  //   console.log('fetching');
+
+  //   try {
+  //     const response = await fetch('/api/test'); // Replace '/api/data' with your API endpoint
+  //     const data = await response.json();
+  //     console.log(data);
+  //   } catch (error) {
+  //     console.error('Error fetching data:', error);
+  //   }
+  // };
+  // fetchData();
+
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const accessToken = urlParams.get('access_token');
+    const refreshToken = urlParams.get('refresh_token');
+
+    console.log('access_token, refreshToken', accessToken, refreshToken);
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <a href="http://localhost:8080/login">Log in to Spotify</a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
