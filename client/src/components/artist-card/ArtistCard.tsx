93b97d84a07f5ad6ea903b5cf050e5b289527c79
Ray Olivier <rayolivier@outlook.com>
2024-01-17 18:40:44 -0600

    cards components to display tracks and artists

---
+++ client/src/components/artist-card/ArtistCard.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { TSpotifyImage } from '../../types/types';
+import './ArtistCard.scss';
+
+type TArtistCardProps = {
+  name: string;
+  images?: TSpotifyImage[];
+};
+
+const ArtistCard: React.FC<TArtistCardProps> = ({ name, images }) => {
+  return (
+    <li className="artist-card">
+      {images && images[0] && (
+        <div className="artist-card__image">
+          <img src={images[0].url} alt="" />
+        </div>
+      )}
+      <h3 className="artist-card__name">{name}</h3>
+    </li>
+  );
+};
+
+export default ArtistCard;
---
+++ client/src/components/track-card/TrackCard.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { TTrack } from '../../types/types';
+import './TrackCard.scss';
+import { formatDuration } from '../../utils';
+
+type TTrackCardProps = {
+  track: TTrack;
+  num?: number;
+};
+
+const TrackCard: React.FC<TTrackCardProps> = ({ track, num }) => {
+  return (
+    <li className="track-card">
+      <div className="track-card__left">
+        {num && <div className="track-card__num">{num}</div>}
+        {track.album.images && track.album.images[0] && (
+          <div className="track-card__image">
+            <img src={track.album.images[0].url} alt="" />
+          </div>
+        )}
+        <span className="track-card__info">
+          <div className="track-card__title">{track.name}</div>
+          <div className="track-card__subtitle">
+            <span className="track-card__artists">{track.artists.map(artist => artist.name)}</span> | {track.album.name}
+          </div>
+        </span>
+      </div>
+
+      <div className="track-card__duration">{formatDuration(track.duration_ms)}</div>
+    </li>
+  );
+};
+
+export default TrackCard;
