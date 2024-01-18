9d619e461e83102bb11828e011374839fb57ab11
Ray Olivier <rayolivier@outlook.com>
2024-01-17 18:45:12 -0600

    fix card imports and exports

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,2 +1,4 @@
+export { default as ArtistCard } from './artist-card/ArtistCard';
+export { default as TrackCard } from './track-card/TrackCard';
 export { default as Login } from './login/Login';
 export { default as Header } from './header/Header';
--- client/src/pages/profile/Profile.tsx
+++ client/src/pages/profile/Profile.tsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
-import { getCurrentUserPlaylists, getCurrentUserProfile } from '../../api/spotify';
-import { TProfile } from '../../types/types';
-import { Header } from '../../components';
-// import { getCurrentUserProfile } from '../spotify';
+import { getCurrentUserPlaylists, getCurrentUserProfile, getTopItems } from '../../api/spotify';
+import { TArtist, TProfile, TTrack } from '../../types/types';
+import { ArtistCard, Header, TrackCard } from '../../components';
+import './Profile.scss';
 
 const Profile = () => {
   const [profile, setProfile] = useState<TProfile | null>(null);
+  const [topArtists, setTopArtists] = useState<TArtist[] | null>(null);
+  const [topTracks, setTopTracks] = useState<TTrack[]>([]);
   // const [playlists, setPlaylists] = useState<TCurrentUserPlaylistData | null>(null);
 
   useEffect(() => {
@@ -17,8 +19,13 @@ const Profile = () => {
         const userPlaylists = await getCurrentUserPlaylists();
         fullProfile.playlists = userPlaylists.data;
 
-        console.log('fullProfile', fullProfile);
+        const topArtists = await getTopItems({ type: 'artists', time_range: 'short_term' });
+        const topTracks = await getTopItems({ type: 'tracks', time_range: 'short_term' });
+        console.log('topTracks', topTracks);
+
         setProfile(fullProfile);
+        setTopArtists(topArtists.data.items as TArtist[]);
+        setTopTracks(topTracks.data.items as TTrack[]);
       } catch (error) {
         console.error(error);
       }
@@ -27,7 +34,31 @@ const Profile = () => {
     fetchData();
   }, []);
 
-  return <>{profile && <Header profile={profile} />}</>;
+  return (
+    <div className="profile">
+      {profile && <Header profile={profile} />}
+      {topArtists && (
+        <div className="section artists">
+          <h2>Top artists this month</h2>
+          <ul className="artist-list">
+            {topArtists.map(artist => (
+              <ArtistCard name={artist.name} images={artist.images} key={artist.id}></ArtistCard>
+            ))}
+          </ul>
+        </div>
+      )}
+      {topTracks && (
+        <div className="section tracks">
+          <h2>Top tracks this month</h2>
+          <ul className="track-list">
+            {topTracks.map((track, i) => (
+              <TrackCard track={track} num={i + 1} key={track.id}></TrackCard>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Profile;
