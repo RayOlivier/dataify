e85a3caadb99a3c96b4e13033a34c0c1db6c26bd
Ray Olivier <rayolivier@outlook.com>
2024-01-17 18:41:24 -0600

    add scope / permission to view spotify user's top listens

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/test', (req, res) => {
 app.get('/login', (req, res) => {
   const state = generateRandomString(16);
   res.cookie(stateKey, state);
-  const scope = 'user-read-private user-read-email';
+  const scope = ['user-read-private', 'user-read-email', 'user-top-read'].join(' ');
 
   const queryParams = querystring.stringify({
     client_id: CLIENT_ID,
