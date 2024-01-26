d553d26b264aa9afde1b0d1ca30164a1d6cb0fc4
Ray Olivier <rayolivier@outlook.com>
2024-01-25 23:54:39 -0600

    add back env

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -1,7 +1,6 @@
 import './Login.scss';
 
-const LOGIN_URI = 'https://dataify.onrender.com/login'; // guessing uri for now
-// const LOGIN_URI = process.env.NODE_ENV !== 'production' ? 'http://localhost:8080/login' : 'https://dataify.onrender.com/login'; // guessing uri for now
+const LOGIN_URI = process.env.NODE_ENV !== 'production' ? 'http://localhost:8080/login' : 'https://dataify.onrender.com/login'; // guessing uri for now
 
 export const Login = () => (
   <div className="login__container">
